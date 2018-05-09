f7736bfc2526d3e8ccc4c3e4e2e7ab8308cd5566
Untitled <john.d.reddock@gmail.com>
2018-05-09 18:16:33 -0400

    added jquery for transitions

--- js/main.js
+++ js/main.js
@@ -2,3 +2,30 @@
 $('.nav a').on('click', (event) => {
     $('.navbar-collapse').collapse('hide');
 })
+
+// Transitions to different parts of page
+$('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+    var target = this.hash;
+    $target = $(target);
+    $('html, body').stop().animate({
+        'scrollTop':  $target.offset().top
+    }, 650, 'swing', function () {
+        window.location.hash = target;
+    });
+});
+
+// Transition for hovering over skill
+$('.skill-group i').hover((event) => {
+    $('.icon-name').filter($(event.target).siblings()).removeClass('hide');
+    $('.skill-group i').not($(event.target)).each( (index, element) => {
+        element = $(element);
+        element.addClass('dull');
+    })
+}, () => {
+    $('.skill-section i').each( (index, element) => {
+        $('.icon-name').filter($(event.target).siblings()).addClass('hide');
+        element = $(element);
+        element.removeClass('dull');
+    })
+});
